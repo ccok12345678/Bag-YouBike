76513232e0f1841b0321b86650ed03cd67704aa3
Karl Yang <ccok12345678@gmail.com>
2021-11-16 17:47:24 +0800

    set navbar, cover

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,18 @@
 import showMap from "./modules/map.js";
+import toggleCover from './modules/toggleCover.js'
 
-
+document.addEventListener('click', (e) => {
+  toggleCover(e);
+})
 
 init();
 
 function init() {
   showMap()
 }
+
+// bs tooltip
+const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
--- src/js/modules/map.js
+++ src/js/modules/map.js
@@ -1,16 +1,23 @@
 export default function showMap() {
   // 設定地圖物件
   const map = L.map('map', {
-    center: [24.6763905,120.9137315], // 地圖中心座標
-    zoom: 13                          // zoom 比例
+    center: [24.6870676,120.9073661], // 地圖中心座標 => 取得使用者位置
+    zoom: 16,                          // zoom 比例
+    zoomControl: false,
   })
   
   L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 16,
+      maxZoom: 20,
+      minzoom: 10,
       id: 'mapbox/streets-v11',
       tileSize: 512,
       zoomOffset: -1,
       accessToken: 'pk.eyJ1IjoiY2NvazEyMzQ1Njc4IiwiYSI6ImNrdm94Z3M2eDB1N2cycHFoeDl3cjdjZTUifQ.fjYGDdcuHXFwRD1Tc2yq6w'
   }).addTo(map);
+
+  L.control.zoom({
+    position: 'topright',
+  }).addTo(map);
+
 }
