8afa3272b9cf931f8f2003f6297089a69582c61b
Karl Yang <ccok12345678@gmail.com>
2021-11-20 22:21:18 +0800

    quick updated

--- src/js/modules/hideCard.js
+++ src/js/modules/hideCard.js
@@ -5,7 +5,7 @@ export default function hideCard(e) {
   let i= 0;
 
   cards.forEach(card => {
-    if (itemID !== `card-item-${i}` && itemID !== `card-item` || itemID === 'card-item-search') {
+    if (itemID !== `card-item-${i}` && itemID !== `card-item` && itemID !== 'card-item-search') {
       card.className = 'card shadow pt-3 pb-4 px-4 visually-hidden';
 
       console.log('hide card');
